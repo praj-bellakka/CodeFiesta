a52c0b8163e5f46e6c4ee142d0cda080c8ca2267
Keng Jit <kengjit123@gmail.com>
2021-06-27 17:35:53 +0800

    Added Meet Features

--- lib/pages/friends_page.dart
+++ lib/pages/friends_page.dart
@@ -3,13 +3,13 @@ import 'package:codefiesta_app/services/reusable_widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class FriendsPage extends StatefulWidget {
+class EventsPage extends StatefulWidget {
 
   @override
-  _FriendsPage createState() => _FriendsPage();
+  _EventsPage createState() => _EventsPage();
 }
 
-class _FriendsPage extends State<FriendsPage> {
+class _EventsPage extends State<EventsPage> {
   //keeps track of the current page displayed
   @override
   Widget build(BuildContext context) {
@@ -17,7 +17,52 @@ class _FriendsPage extends State<FriendsPage> {
       body: Column(
         children: [
           SizedBox(height: 50),
+          // Title
           ReusableTitleWidget(title: "Events", fontsize: 40, color: Colors.black),
+          // Tags
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Row(children: [
+                InkWell(
+                  child: ReusableTagWidget(title: 'Events',),
+                ),
+                SizedBox(width: 10), //Spacing between tags
+                InkWell(
+                  child: ReusableTagWidget(title: "Interest Groups",),
+                ),
+              ],)
+          ),
+
+          SizedBox(height: 10),
+          // Search Bar
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black38,
+                  blurRadius: 25,
+                  offset: const Offset(0, 10),
+                ),
+              ],
+            ),
+            child: TextField(
+                onTap: () {},
+                readOnly: true,
+                decoration: InputDecoration(
+                  labelText: "What are you looking for?",
+                  contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                  prefixIcon: Icon(Icons.search),
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                )),
+          ),
+
         ],
     ));
   }
