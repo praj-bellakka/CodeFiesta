6f2fd5294073d843b6e947971bb2546fec6079df
praj-bellakka <61443313+praj-bellakka@users.noreply.github.com>
2021-06-27 22:38:23 +0800

    Cleaned up code

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -17,18 +17,6 @@ class AuthService {
     return _auth.authStateChanges().map(_userFromFirebaseUser);
   }
 
-  //sign in anon
-  Future signInAnon() async {
-    try {
-      UserCredential result = await _auth.signInAnonymously();
-      User user = result.user;
-      return _userFromFirebaseUser(user);
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
-  }
-
   //sign in with email
   Future signInWithEmailAndPassword(String email, String password) async {
     try {
